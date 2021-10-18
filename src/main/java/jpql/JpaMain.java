429bda342afc6e02f3d08804dfe7121571d88d9f
taem1n1 <youman7953@naver.com>
2021-10-18 17:58:52 +0900

    기본 매핑

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,2 +1,30 @@
-package jpql;public class JpaMain {
+package jpql;
+
+import javax.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit();
+        }
+        catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
 }
--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -1,2 +1,22 @@
-package jpql;public class Member {
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    private String username;
+    private int age;
 }
